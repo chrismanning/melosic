4800db4dbc7a7a5bbae77c3001be4b5323b92738
Christian Manning <cmanning999@gmail.com>
2012-07-24 00:41:09 +0100

    Modify flac decoder to use new things

--- src/melosic/plugins/flac/flac.cpp
+++ src/melosic/plugins/flac/flac.cpp
@@ -18,6 +18,9 @@
 #include <melosic/managers/common.hpp>
 #include <FLAC++/decoder.h>
 #include <string.h>
+#include <boost-1_49/boost/shared_ptr.hpp>
+#include <cstdlib>
+#include <cstdio>
 #include <iostream>
 
 class FlacDecoderImpl : public FLAC::Decoder::File {
@@ -25,21 +28,44 @@ public:
     FlacDecoderImpl(IInputSource& dec_) : dec(dec_) {}
     virtual ::FLAC__StreamDecoderWriteStatus write_callback(const ::FLAC__Frame *frame, const FLAC__int32 * const buffer[])
     {
-        for(unsigned i=0; i<frame->header.blocksize; i++) {
-            for(unsigned j=0; j<frame->header.channels; j++) {
-                //FIXME: don't use output range directly once output interface is devised
-                if(dec.getOutputRange()->put((short)buffer[j][i], frame->header.bits_per_sample))
-                    continue;
-                else
-                    return FLAC__STREAM_DECODER_WRITE_STATUS_ABORT;
+        ubyte * tmp = (ubyte *)malloc(frame->header.blocksize * sizeof(uint) * frame->header.channels / (frame->header.bits_per_sample/8));
+        ubyte * buf = tmp;
+
+        if(tmp == NULL) {
+            return FLAC__STREAM_DECODER_WRITE_STATUS_ABORT;
+        }
+
+        for(unsigned i=0,u=0; i<frame->header.blocksize && u<(frame->header.blocksize*2); i++) {
+            for(unsigned j=0; j<frame->header.channels; j++,u++) {
+                switch(frame->header.bits_per_sample/8) {
+                case 1:
+                    *tmp = (ubyte)(buffer[j][i]);
+                    tmp++;
+                    break;
+                case 2:
+                    *(ushort*)tmp = (ushort)(buffer[j][i]);
+                    tmp+=sizeof(ushort);
+                    break;
+                case 3:
+                    *(uint*)tmp = (uint)(buffer[j][i] & 0xFFFFFF);
+                    tmp+=3;
+                    break;
+                case 4:
+                    *(uint*)tmp = (uint)(buffer[j][i]);
+                    tmp+=sizeof(uint);
+                    break;
+                }
             }
         }
+
+        dec.writeBuf(buf, frame->header.blocksize * frame->header.channels * (frame->header.bits_per_sample/8));
         return FLAC__STREAM_DECODER_WRITE_STATUS_CONTINUE;
     }
 
     virtual void metadata_callback(const ::FLAC__StreamMetadata *metadata);
 
     virtual void error_callback(::FLAC__StreamDecoderErrorStatus status) {
+        //FIXME: use future error handling capabilities
         std::cerr << "Got error callback:" << FLAC__StreamDecoderErrorStatusString[status] << std::endl;
     }
 
@@ -49,13 +75,16 @@ private:
 
 class FlacDecoder : public IInputSource {
 public:
-    FlacDecoder() : fd(new FlacDecoderImpl(*this)) {
+    FlacDecoder() : as(0), fd(new FlacDecoderImpl(*this)), buf(new Buffer) {
     }
 
     ~FlacDecoder() {
+        fprintf(stderr, "Flac decoder being destroyed\n");
         fd->finish();
         delete fd;
-        delete as;
+        if(as) {
+            delete as;
+        }
     }
 
     virtual bool canOpen(const char * extension) {
@@ -67,37 +96,38 @@ public:
         fd->process_until_end_of_metadata();
     }
 
-    virtual void initOutput(IOutputRange * output_) {
-        output = output_;
-    }
-
-    virtual IOutputRange * getOutputRange() {
-        return output;
+    void writeBuf(void * ptr, size_t length) {
+        if(buf->ptr()) {
+            free(const_cast<void*>(buf->ptr()));
+            buf->ptr(0);
+        }
+        buf->ptr(ptr);
+        buf->length(length);
     }
 
     virtual AudioSpecs getAudioSpecs() {
         return *as;
     }
 
-    virtual DecodeRange * opSlice();
+    virtual DecodeRange * getDecodeRange();
 
     AudioSpecs * as;
     FlacDecoderImpl * fd;
-    unsigned * buf;
-    IOutputRange * output;
+    IBuffer * buf;
 };
 
 void FlacDecoderImpl::metadata_callback(const ::FLAC__StreamMetadata *metadata)
 {
-    /* print some stats */
     if(metadata->type == FLAC__METADATA_TYPE_STREAMINFO) {
-        auto tmp = new AudioSpecs(get_channels(),get_bits_per_sample(),get_total_samples(),get_sample_rate());
+        //the get*() functions don't seem to work at this point
+        FLAC__StreamMetadata_StreamInfo m = metadata->data.stream_info;
+        auto tmp = new AudioSpecs(m.channels, m.bits_per_sample, m.sample_rate, m.total_samples);
         reinterpret_cast<FlacDecoder&>(dec).as = tmp;
 
-        fprintf(stderr, "sample rate    : %u Hz\n", tmp->sample_rate);
-        fprintf(stderr, "channels       : %u\n", tmp->channels);
-        fprintf(stderr, "bits per sample: %u\n", tmp->bps);
-        fprintf(stderr, "total samples  : %lu\n", tmp->total_samples);
+        printf("sample rate    : %u Hz\n", tmp->sample_rate);
+        printf("channels       : %u\n", tmp->channels);
+        printf("bits per sample: %u\n", tmp->bps);
+        printf("total samples  : %lu\n", tmp->total_samples);
     }
 }
 
@@ -106,32 +136,37 @@ public:
     FlacRange(FlacDecoder * dec) : dec(dec) {
     }
 
-    ~FlacRange() {
-        delete dec;
-    }
-
-    virtual unsigned * front() {
+    virtual IBuffer * front() {
         return dec->buf;
     }
 
     virtual void popFront() {
-        printf("in popFront (C++)\n");
-        dec->fd->process_single();
+        if(!dec->fd->process_single()) {
+            std::cerr << "Decode aborted" << std::endl;
+        }
     }
 
     virtual bool empty() {
         auto state = dec->fd->get_state();
-        return state == FLAC__STREAM_DECODER_END_OF_STREAM;
+        return state == FLAC__STREAM_DECODER_END_OF_STREAM || state == FLAC__STREAM_DECODER_ABORTED;
+    }
+
+    virtual size_t length() {
+        return dec->fd->get_total_samples();
     }
 
 private:
     FlacDecoder * dec;
 };
 
-DecodeRange * FlacDecoder::opSlice() {
+DecodeRange * FlacDecoder::getDecodeRange() {
     return new FlacRange(this);
 }
 
-extern "C" void registerPlugin(IKernel * k) {
+extern "C" void registerPluginObjects(IKernel * k) {
     k->getInputManager()->addInputSource(new FlacDecoder);
+}
+
+extern "C" void destroyPluginObjects() {
+    fprintf(stderr, "Destroying flac plugin objects\n");
 }
