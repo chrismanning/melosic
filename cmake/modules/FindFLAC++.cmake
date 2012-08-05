71c7b854bafae9bcae54c45581e2c95bd794975d
Christian Manning <cmanning999@gmail.com>
2012-08-05 19:28:02 +0100

    Use cmake better so flac can work on windows + to only try to build alsa on linux

---
+++ cmake/modules/FindFLAC++.cmake
@@ -0,0 +1,28 @@
+# - Try to find Flac, the Free Lossless Audio Codec
+# Once done this will define
+#
+#  FLAC_FOUND - system has Flac
+#  FLAC_INCLUDE_DIR - the Flac include directory
+#  FLAC_LIBRARIES - Link these to use Flac
+#  FLAC_OGGFLAC_LIBRARIES - Link these to use OggFlac
+#
+# No version checking is done - use FLAC_API_VERSION_CURRENT to
+# conditionally compile version-dependent code
+
+# Copyright (c) 2006, Laurent Montel, <montel@kde.org>
+# Copyright (c) 2006, Alexander Neundorf, <neundorf@kde.org>
+#
+# Redistribution and use is allowed according to the terms of the BSD license.
+# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+
+FIND_PATH(FLAC++_INCLUDE_DIR FLAC++/metadata.h)
+
+message(STATUS "CMAKE_INCLUDE_PATH: " ${CMAKE_INCLUDE_PATH})
+
+FIND_LIBRARY(FLAC++_LIBRARIES NAMES FLAC++ )
+
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(Flac++  REQUIRED_VARS  FLAC++_LIBRARIES FLAC++_INCLUDE_DIR)
+
+# show the FLAC_INCLUDE_DIR and FLAC_LIBRARIES variables only in the advanced view
+MARK_AS_ADVANCED(FLAC++_INCLUDE_DIR FLAC++_LIBRARIES)
