c6310c859d4c7c81b8a2092f2418750dd9f1fe4b
Christian Manning <cmanning999@gmail.com>
2012-08-08 17:39:16 +0100

    Use stream-like api in flac plugin + use circular buffer

--- src/melosic/plugins/flac/flac.cpp
+++ src/melosic/plugins/flac/flac.cpp
@@ -21,18 +21,58 @@
 using boost::factory;
 #include <boost/format.hpp>
 using boost::format;
+#include <boost/circular_buffer.hpp>
+using boost::circular_buffer;
 #include <vector>
 
 #include <melosic/common/common.hpp>
 using namespace Melosic;
 
-class FlacDecoderImpl : public FLAC::Decoder::File {
+class FlacDecoder : public Input::IFileSource, private FLAC::Decoder::File {
 public:
-    FlacDecoderImpl(IInputSource& dec_) : dec(dec_) {}
-    virtual ::FLAC__StreamDecoderWriteStatus write_callback(const ::FLAC__Frame *frame, const FLAC__int32 * const buffer[])
-    {
-        auto buf = std::vector<uint8_t>(frame->header.blocksize * sizeof(uint32_t) * frame->header.channels / (frame->header.bits_per_sample/8));
-        auto tmp = &buf[0];
+    FlacDecoder() : circ_buf(65536) {}
+
+    virtual ~FlacDecoder() {
+        cerr << "Flac decoder being destroyed\n" << endl;
+        finish();
+    }
+
+    virtual std::streamsize read(uint8_t * s, std::streamsize n) {
+        while((std::streamsize)circ_buf.size() < n && !end()) {
+            process_single();
+        }
+        std::streamsize i;
+        for(i = 0; i<n && !circ_buf.empty(); i++) {
+            s[i] = circ_buf.front();
+            circ_buf.pop_front();
+        }
+        return i;
+    }
+
+    virtual void openFile(const std::string& filename) {
+        init(filename);
+        process_until_end_of_metadata();
+    }
+
+    virtual const AudioSpecs& getAudioSpecs() {
+        return as;
+    }
+
+private:
+    bool end() {
+        auto state = (::FLAC__StreamDecoderState)get_state();
+        return state == FLAC__STREAM_DECODER_END_OF_STREAM || state == FLAC__STREAM_DECODER_ABORTED;
+    }
+
+    virtual ::FLAC__StreamDecoderWriteStatus write_callback(const ::FLAC__Frame *frame,
+                                                            const FLAC__int32 * const buffer[]) {
+        auto bytes = frame->header.blocksize
+                     * sizeof(int32_t)
+                     * frame->header.channels
+                     / (frame->header.bits_per_sample/8);
+
+        std::vector<uint8_t> buf_(bytes);
+        auto tmp = &buf_[0];
 
         if(tmp == NULL) {
             return FLAC__STREAM_DECODER_WRITE_STATUS_ABORT;
@@ -41,83 +81,56 @@ public:
         for(unsigned i=0,u=0; i<frame->header.blocksize && u<(frame->header.blocksize*2); i++) {
             for(unsigned j=0; j<frame->header.channels; j++,u++) {
                 switch(frame->header.bits_per_sample/8) {
-                case 1:
-                    *tmp = (uint8_t)(buffer[j][i]);
-                    tmp++;
-                    break;
-                case 2:
-                    *(uint16_t*)tmp = (uint16_t)(buffer[j][i]);
-                    tmp+=sizeof(uint16_t);
-                    break;
-                case 3:
-                    *(uint32_t*)tmp = (uint32_t)(buffer[j][i] & 0xFFFFFF);
-                    tmp+=3;
-                    break;
-                case 4:
-                    *(uint32_t*)tmp = (uint32_t)(buffer[j][i]);
-                    tmp+=sizeof(uint32_t);
-                    break;
+                    case 1:
+                        circ_buf.push_back((uint8_t)(buffer[j][i]));
+                        break;
+                    case 2:
+                        circ_buf.push_back((uint8_t)(buffer[j][i]));
+                        circ_buf.push_back((uint8_t)(buffer[j][i] >> 8));
+                        break;
+                    case 3:
+                        circ_buf.push_back((uint8_t)(buffer[j][i]));
+                        circ_buf.push_back((uint8_t)(buffer[j][i] >> 8));
+                        circ_buf.push_back((uint8_t)(buffer[j][i] >> 16));
+                        break;
+                    case 4:
+                        circ_buf.push_back((uint8_t)(buffer[j][i]));
+                        circ_buf.push_back((uint8_t)(buffer[j][i] >> 8));
+                        circ_buf.push_back((uint8_t)(buffer[j][i] >> 16));
+                        circ_buf.push_back((uint8_t)(buffer[j][i] >> 24));
+                        break;
                 }
             }
         }
 
-        dec.writeBuf(buf, frame->header.blocksize * frame->header.channels * (frame->header.bits_per_sample/8));
+        buf = std::move(buf_);
+
         return FLAC__STREAM_DECODER_WRITE_STATUS_CONTINUE;
     }
-
-    virtual void metadata_callback(const ::FLAC__StreamMetadata *metadata);
 
     virtual void error_callback(::FLAC__StreamDecoderErrorStatus status) {
         //FIXME: use future error handling capabilities
         cerr << "Got error callback:" << FLAC__StreamDecoderErrorStatusString[status] << endl;
     }
 
-private:
-    IInputSource& dec;
-};
+    virtual void metadata_callback(const ::FLAC__StreamMetadata *metadata) {
+        if(metadata->type == FLAC__METADATA_TYPE_STREAMINFO) {
+            //the get*() functions don't seem to work at this point
+            FLAC__StreamMetadata_StreamInfo m = metadata->data.stream_info;
 
-class FlacDecoder : public IInputSource {
-public:
-    FlacDecoder() : fd(*this) {
-    }
+            as = AudioSpecs(m.channels, m.bits_per_sample, m.sample_rate, m.total_samples);
 
-    ~FlacDecoder() {
-        cerr << "Flac decoder being destroyed\n" << endl;
-        fd.finish();
-    }
-
-    virtual void openFile(const std::string& filename) {
-        fd.init(filename);
-        fd.process_until_end_of_metadata();
-    }
-
-    void writeBuf(const std::vector<uint8_t>& buf, size_t length) {
-        this->buf = buf;
-    }
-
-    virtual const AudioSpecs& getAudioSpecs() {
-        return as;
+            cout << format("sample rate    : %u Hz") % as.sample_rate << endl;
+            cout << format("channels       : %u") % as.channels << endl;
+            cout << format("bits per sample: %u") % as.bps << endl;
+            cout << format("total samples  : %lu") % as.total_samples << endl;
+        }
     }
 
     AudioSpecs as;
-    FlacDecoderImpl fd;
     std::vector<uint8_t> buf;
+    circular_buffer<uint8_t> circ_buf;
 };
-
-void FlacDecoderImpl::metadata_callback(const ::FLAC__StreamMetadata *metadata)
-{
-    if(metadata->type == FLAC__METADATA_TYPE_STREAMINFO) {
-        //the get*() functions don't seem to work at this point
-        FLAC__StreamMetadata_StreamInfo m = metadata->data.stream_info;
-        AudioSpecs tmp(m.channels, m.bits_per_sample, m.sample_rate, m.total_samples);
-        static_cast<FlacDecoder&>(dec).as = tmp;
-
-        cout << format("sample rate    : %u Hz") % tmp.sample_rate << endl;
-        cout << format("channels       : %u") % tmp.channels << endl;
-        cout << format("bits per sample: %u") % tmp.bps << endl;
-        cout << format("total samples  : %lu") % tmp.total_samples << endl;
-    }
-}
 
 extern "C" void registerPluginObjects(IKernel& k) {
     k.getInputManager().addFactory(factory<std::shared_ptr<FlacDecoder>>(), {".flac"});
