4adace1c78ff10e8d2ff8c035254ecaf1ba11950
Christian Manning <cmanning999@gmail.com>
2014-03-12 17:36:50 +0000

    Create an empty test case for CategoryProxyModel using Qt Test

---
+++ src/melosic/gui/test/categoryproxymodel_test.cpp
@@ -0,0 +1,32 @@
+/**************************************************************************
+**  Copyright (C) 2013 Christian Manning
+**
+**  This program is free software: you can redistribute it and/or modify
+**  it under the terms of the GNU General Public License as published by
+**  the Free Software Foundation, either version 3 of the License, or
+**  (at your option) any later version.
+**
+**  This program is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+**
+**  You should have received a copy of the GNU General Public License
+**  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**************************************************************************/
+
+#include <melosic/gui/categoryproxymodel.hpp>
+#include <melosic/gui/category.hpp>
+
+#include <QtTest/QtTest>
+
+struct CategoryProxyModelTest : QObject {
+    Q_OBJECT
+private Q_SLOTS:
+    void Test1() {
+        QVERIFY(false);
+    }
+};
+
+QTEST_MAIN(CategoryProxyModelTest)
+#include "categoryproxymodel_test.moc"
