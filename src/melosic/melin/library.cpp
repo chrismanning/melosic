e2a0ef52a56747e03fbd5fe30c941932560f9f13
Christian Manning <cmanning999@gmail.com>
2014-05-19 22:13:51 +0100

    Add index for "metadata" field and optimise all indexes

--- src/melosic/melin/library.cpp
+++ src/melosic/melin/library.cpp
@@ -162,17 +162,24 @@ Manager::impl::impl(Config::Manager& confman, Decoder::Manager& decman, Thread::
             return;
         }
         assert(coll);
-        coll.set_index("modified", ejdb::index_mode::number|ejdb::index_mode::rebuild, ec);
+        coll.set_index("modified", ejdb::index_mode::number|ejdb::index_mode::rebuild|ejdb::index_mode::optimize, ec);
         if(ec) {
             ERROR_LOG(logject) << "Could not set index on collection field \"modified\": " << ec.message();
             ec.clear();
         }
 
-        coll.set_index("location", ejdb::index_mode::string|ejdb::index_mode::rebuild, ec);
+        coll.set_index("location", ejdb::index_mode::string|ejdb::index_mode::rebuild|ejdb::index_mode::optimize, ec);
         if(ec) {
             ERROR_LOG(logject) << "Could not set index on collection field \"location\": " << ec.message();
             ec.clear();
         }
+
+        coll.set_index("metadata", ejdb::index_mode::array|ejdb::index_mode::rebuild|ejdb::index_mode::optimize, ec);
+        if(ec) {
+            ERROR_LOG(logject) << "Could not set index on collection field \"metadata\": " << ec.message();
+            ec.clear();
+        }
+
         TRACE_LOG(logject) << "Indexes set on \"tracks\" collection";
     });
 }
