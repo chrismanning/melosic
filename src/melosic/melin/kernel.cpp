a3cd89230eed3df28c820c597298aa871cc13c7d
Christian Manning <cmanning999@gmail.com>
2014-05-20 21:54:39 +0100

    Use asio signal handlers & at(_quick_)exit to mitigate db corruption

--- src/melosic/melin/kernel.cpp
+++ src/melosic/melin/kernel.cpp
@@ -16,6 +16,7 @@
 **************************************************************************/
 
 #include <asio/io_service.hpp>
+#include <asio/signal_set.hpp>
 
 #include <melosic/melin/kernel.hpp>
 #include <melosic/melin/config.hpp>
@@ -29,6 +30,7 @@
 #include <melosic/common/directories.hpp>
 #include <melosic/core/track.hpp>
 #include <melosic/melin/library.hpp>
+#include <melosic/melin/logging.hpp>
 
 namespace Melosic {
 namespace Core {
@@ -38,6 +40,7 @@ struct Kernel::impl {
         confman("melosic.conf"),
         plugman(confman),
         io_service(),
+        signal_set(io_service),
         outman(confman, io_service),
         null_worker(new asio::io_service::work(io_service)),
         tman(&io_service),
@@ -47,11 +50,29 @@ struct Kernel::impl {
         encman(),
         libman(confman, decman, plugman, tman),
         playlistman()
-    {}
+    {
+        signal_set.add(SIGABRT);
+        signal_set.add(SIGINT);
+        signal_set.add(SIGTERM);
+        signal_set.add(SIGQUIT);
+        signal_set.async_wait([this](asio::error_code ec, int signo) {
+            if(ec)
+                return;
+            WARN_LOG(logject) << "SIGNAL RAISED: " << signo;
+            if(signo == SIGABRT)
+                std::quick_exit(signo);
+            std::exit(signo);
+        });
+    }
+
+    ~impl() {
+        signal_set.cancel();
+    }
 
     Config::Manager confman;
     Plugin::Manager plugman;
     asio::io_service io_service;
+    asio::signal_set signal_set;
     Output::Manager outman;
     std::unique_ptr<asio::io_service::work> null_worker;
     Thread::Manager tman;
@@ -61,6 +82,7 @@ struct Kernel::impl {
     Encoder::Manager encman;
     Library::Manager libman;
     Melosic::Playlist::Manager playlistman;
+    Logger::Logger logject{logging::keywords::channel = "Kernel"};
 };
 
 Kernel::Kernel() : pimpl(new impl) {}
