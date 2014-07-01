ed06e73be4ad01f6c7ff2d1e1bcea0f5558008c1
Christian Manning <cmanning999@gmail.com>
2014-07-01 17:15:53 +0100

    Add default_executor which returns a thread pool executor

---
+++ src/melosic/executors/default_executor.cpp
@@ -0,0 +1,29 @@
+/**************************************************************************
+**  Copyright (C) 2014 Christian Manning
+**
+**  This program is free software: you can redistribute it and/or modify
+**  it under the terms of the GNU General Public License as published by
+**  the Free Software Foundation, either version 3 of the License, or
+**  (at your option) any later version.
+**
+**  This program is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+**
+**  You should have received a copy of the GNU General Public License
+**  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**************************************************************************/
+
+#include "default_executor.hpp"
+
+namespace Melosic {
+namespace executors {
+
+std::shared_ptr<default_executor_t> default_executor() {
+    static std::shared_ptr<default_executor_t> ex = std::make_shared<default_executor_t>();
+    return ex;
+}
+
+} // namespace executors
+} // namespace Melosic
